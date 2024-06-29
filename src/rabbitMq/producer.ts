d6776fe07f073c2ae12dccc2e078c6b2d59f4924
rashi9551 <rashidroz1361@gmail.com>
2024-06-29 19:13:43 +0530

    the user side payment completed

--- src/rabbitMq/producer.ts
+++ src/rabbitMq/producer.ts
@@ -3,7 +3,7 @@ import {Channel} from 'amqplib'
 export default class Producer{
     constructor(private channel: Channel) {}
 
-    async produceMessages(data: any, correlationId: string, replyToQueue: string){
+    async produceMessages(data: any, correlationId: string, replyToQueue: string){        
         this.channel.sendToQueue(replyToQueue, Buffer.from(JSON.stringify(data)), {
             correlationId: correlationId
         });
